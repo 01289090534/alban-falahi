52d3a06c8ca53fecc657dc54a8cb321a8d6b97aa
01289090534 <nocommentaly2015@gmail.com>
2026-09-13 15:44:01 +0300

    Use Alban Falahi logo as app icon

--- hr-system/src/main.tsx
+++ hr-system/src/main.tsx
@@ -3,5 +3,11 @@ import {createRoot} from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import './styles.css';
+import logoData from './assets/logoData';
 
+const icon=document.querySelector<HTMLLinkElement>('link[rel="icon"]')||document.createElement('link');
+icon.rel='icon';icon.href=logoData;document.head.appendChild(icon);
+
+document.documentElement.dir='rtl';
+document.documentElement.lang='ar';
 createRoot(document.getElementById('root')!).render(<React.StrictMode><BrowserRouter><App/></BrowserRouter></React.StrictMode>);
