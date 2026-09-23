f813480d23c87b198d9e42b087f1ce08091db359
01289090534 <nocommentaly2015@gmail.com>
2026-09-23 18:21:00 +0300

    Remove dashboard function redirect loop

--- functions/dashboard.js
+++
@@ -1,8 +0,0 @@
-export async function onRequest(context) {
-  const url = new URL(context.request.url);
-  if (url.pathname === '/dashboard' || url.pathname === '/dashboard/') {
-    const dashboardUrl = new URL('/dashboard/index.html', url);
-    return context.env.ASSETS.fetch(new Request(dashboardUrl, context.request));
-  }
-  return context.next();
-}
